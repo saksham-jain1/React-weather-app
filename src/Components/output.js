3b5e0f3879d981bba80afc18eaff282461fb774d
saksham-jain1 <sj20011002@gmail.com>
2022-06-05 16:02:15 +0530

    final

--- src/Components/output.js
+++ src/Components/output.js
@@ -1,42 +1,65 @@
 import React, {useEffect, useState} from 'react'
+import { FaTemperatureLow , FaTemperatureHigh } from "react-icons/fa";
 
 function Output()
 {
-    const [temp,setTemp]=useState('No Data Found');
-    const [place,setPlace]=useState('Jhansi'); 
-    
-    
+    const [data,setData]=useState([]);
+    const [place,setPlace]=useState('jhansi');
+    const [imgUrl,setImgUrl]=useState('');
+    const [weather,setweather]=useState('');
     useEffect(()=>{
         const getWeather = async () => {
+            if(place!=='')
+            {
             const url =`https://api.openweathermap.org/data/2.5/weather?q=${place}&units=metric&appid=6b211ad57ccbde222a34f29288667216`;
             const response = await fetch(url);
             const resjson = await response.json();
-            console.log(resjson);
-            const main = resjson.main; 
-            if(main)
+            const weather = resjson.weather;
+            if(resjson.main)
             {
-                setTemp(main.temp);
+                setData(resjson.main);
+                setweather(weather[0])
+                setImgUrl(`http://openweathermap.org/img/wn/${weather[0].icon}@2x.png`);
             }
             else
             {
-                setTemp('No Data Found');
+                setData([]);
+                setweather('');
+                setImgUrl('');
             }
+        }
         }
         getWeather();
     },[place]);
 
     return  (
-        <center>
-        <div className="card bg-primary m-5 p-3 col-sm-5 shadow-lg">
-        <div className="p-2 mb-4">
-        <input className="form-control me-2" type="search" onChange={(e) => {setPlace(e.target.value)}} value={place} />
+        <div className="card col-md-6 container-fluid align-center justify-content-center bg-info shadow-lg my-5 p-5" style={{borderRadius:'25px'}}>
+        <div className="p-2 m-2">
+        <input className="form-control me-2" type="search" placeholder='Enter city name' onChange={(e) => {setPlace(e.target.value)}} value={place} />
         </div>
-                <>
-                <div><h2> {place}</h2></div>
-                <div><h3>Temperature: {temp}</h3></div>                    
-                </>
+                <div className='col-10 p-3 mx-5'><h2 style={{textTransform:'capitalize',textAlign:'center'}}> <img src={imgUrl} alt='' />&emsp; {place}<br />{weather.description}</h2></div>
+                {data!==''?
+                <table>
+                <tbody>
+                <tr>
+                <td className='col-5'>
+                    <h3>Temperature: {data.temp}</h3>
+                    <FaTemperatureLow size={20} /> Min Temperature :&emsp;{data.temp_min}
+                    <br />
+                    <FaTemperatureHigh size={20} /> Max Temperature :&emsp;{data.temp_max}
+                </td>
+                <td className='col-5'>
+                    <h3>Temperature: {data.temp}</h3>
+                    <FaTemperatureLow size={20} /> Humidity :&emsp;{data.humidity}
+                    <br />
+                    <FaTemperatureHigh size={20} /> Pressure :&emsp;{data.pressure}
+                </td>
+                </tr>
+                </tbody>
+                </table>
+                : <h2> No Data Found </h2>
+                }              
         </div>
-        </center>
     );
 }
 
